7148b2a94b5825ea3e182422f741acf0eb55c4cf
arturveres <artur8118@gmail.com>
2017-07-18 13:54:07 +0300

    Trying to get it to work

--- FoodBot/views.py
+++ FoodBot/views.py
@@ -10,19 +10,20 @@ def handle_verification():
 
 def reply(user_id, msg):
     data = {
-        "recipient": {"id": "100003476135788"},
+        "recipient": {"id": user_id},
         "message": {"text": msg}
     }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
+    resp = requests.post("https://graph.facebook.com/v2.9/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
                          json=data)
     print(resp.content)
 
 
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
+    print(request, request.json)
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
+    message = data['entry'][0]['messaging'][0]['message'].get('text')
     reply(sender, message[::-1])
 
     return "ok"
