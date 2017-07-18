ea4fbb19d7e59fb840072415cd53c1c64900b110
arturveres <artur8118@gmail.com>
2017-07-18 14:00:11 +0300

    Trying to get it to work

--- FoodBot/views.py
+++ FoodBot/views.py
@@ -8,10 +8,10 @@ def handle_verification():
     return request.args.get('hub.challenge') or 'Hello, world'
 
 
-def reply(user_id, msg):
+def reply(user_id, msg, type):
     data = {
         "recipient": {"id": user_id},
-        "message": {"text": msg}
+        "message": {"text" if type == 'message' else 'sticker_id': msg}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
                          json=data)
@@ -23,7 +23,8 @@ def handle_incoming_messages():
     print(request, request.json)
     data = request.json
     sender = data['entry'][0].get('messaging')[0].get('sender').get('id')
-    message = data['entry'][0].get('messaging')[0].get('message').get('text')
-    reply(sender, message[::-1])
+    message = data['entry'][0].get('messaging')[0].get('message').get('text') or  data['entry'][0].get('messaging')[0].get('message').get('sticker_id')
+    typemsg = "message" if 'sticker_id' not in  data['entry'][0].get('messaging')[0].get('message') else "sticker"
+    reply(sender, message, typemsg)
 
     return "ok"
