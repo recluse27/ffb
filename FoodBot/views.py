6cec4b77c90991abe308b6382f224944222a484c
arturveres <artur8118@gmail.com>
2017-07-19 14:59:20 +0300

    constructor for messages

--- FoodBot/views.py
+++ FoodBot/views.py
@@ -1,6 +1,7 @@
 from FoodBot import app
 from flask import request
 import requests
+import time
 
 
 @app.route('/', methods=['GET'])
@@ -15,16 +16,66 @@ def reply(user_id, msg, type):
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
                          json=data)
-    print(resp.content)
+    print(resp.text)
 
 
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
-    print(request, request.json)
+    print(request.json)
     data = request.json
     sender = data['entry'][0].get('messaging')[0].get('sender').get('id')
-    message = data['entry'][0].get('messaging')[0].get('message').get('text') or  data['entry'][0].get('messaging')[0].get('message').get('sticker_id')
-    typemsg = "message" if 'sticker_id' not in  data['entry'][0].get('messaging')[0].get('message') else "sticker"
-    reply(sender, message, typemsg)
+    message = data['entry'][0].get('messaging')[0].get('message').get('text') or data['entry'][0].get('messaging')[
+        0].get('message').get('sticker_id')
+    message_type = "message" if 'sticker_id' not in data['entry'][0].get('messaging')[0].get('message') else "sticker"
+    reply(sender, message, message_type)
 
     return "ok"
+
+
+def make_message_content(data):
+    if 'delivery' in data[0]:
+        return None
+    if 'attachment' in data[0]:
+        pass
+
+
+def make_quick_replies():
+    pass
+
+
+def make_buttons():
+    pass
+
+
+def make_receipt(userid, elements):
+    receipt = {
+        "recipient": {
+            "id": userid
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "receipt",
+                    "recipient_name": "",
+                    "order_number": "",
+                    "currency": "UAH",
+                    "payment_method": "Visa",
+                    "order_url": "",
+                    "timestamp": round(time.time()),
+                    "elements": elements,
+                    "address": {
+                        "street_1": "1 Hacker Way",
+                        "city": "Menlo Park",
+                        "postal_code": "94025",
+                        "state": "CA",
+                        "country": "US"
+                    },
+                    "summary": {
+                        "total_cost": 56.14
+                    }
+                }
+            }
+        }
+    }
+    return receipt
