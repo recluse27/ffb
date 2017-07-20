0516fd4af8a382bed8af809f73067679a5474854
arturveres <artur8118@gmail.com>
2017-07-20 13:55:38 +0300

    Added templates

--- FoodBot/views.py
+++ FoodBot/views.py
@@ -4,12 +4,13 @@ from flask import request
 import requests
 import time
 
+
 @app.route('/', methods=['GET'])
 def handle_verification():
     return request.args.get('hub.challenge') or 'Hello, world'
 
 
-def reply(user_id, payload):
+def reply_with_attachment(user_id, payload):
     data = {
         "recipient": {"id": user_id},
         "message": {"attachment": payload,
@@ -17,6 +18,18 @@ def reply(user_id, payload):
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
                          json=data)
+    print(resp.text)
+
+
+def reply_with_message(user_id):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"message": {"text": "Сделай свой выбор"},
+                    "quick_replies": QUICK_REPLIES}
+    }
+    resp = requests.post(
+        "https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
+        json=data)
     print(resp.text)
 
 
@@ -33,8 +46,11 @@ def handle_incoming_messages():
             payload = RECEIPT_TEMPLATE(item)
         if payload:
             sender = data['entry'][0].get('messaging')[0].get('sender').get('id')
-            reply(sender, payload)
+            reply_with_attachment(sender, payload)
+    else:
+
     return "ok"
+
 
 def find_in_list(title, items):
     for item in items:
