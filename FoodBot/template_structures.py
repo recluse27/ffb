20d56b3e4b06489a65a72a451e6aeee9d1e3707f
arturveres <artur8118@gmail.com>
2017-07-20 13:39:44 +0300

    Added templates

--- FoodBot/template_structures.py
+++ FoodBot/template_structures.py
@@ -1,8 +1,81 @@
-LIST_ELEMENT = {
-    "title": "",
-    "subtitle": "",
-    "quantity": "",
-    "price": "",
-    "currency": "UAH",
-    "image_url": ""
+import time
+
+PRODUCT_LIST = lambda items: {
+    "type": "template",
+    "payload": {
+        "template_type": "list",
+        "elements": [
+            {
+                "title": item['title'],
+                "subtitle": str(item['price']) + ' UAH',
+                "image_url": item['image_url'],
+                "default_action": {
+                    "type": "postback",
+                    "payload": {"type": "get_receipt",
+                                "item_name": item['title']}
+                },
+                "buttons": [
+                    {
+                        "title": "Buy",
+                        "type": "postback",
+                        "payload": {"type": "get_receipt",
+                                    "item_name": item['title']}
+                    }
+                ]
+            } for item in items]
+    }
 }
+RECEIPT_TEMPLATE = lambda item: {
+    "type": "template",
+    "payload": {
+        "template_type": "receipt",
+        "recipient_name": "Stephane Crozatier",
+        "order_number": round(time.time()),
+        "currency": "UAH",
+        "payment_method": "Credit Card",
+        "order_url": "",
+        "timestamp": round(time.time()),
+        "elements": [
+            RECEIPT_ELEMENT_TEMPLATE(item=item)
+        ],
+        "address": {
+            "street_1": "1 Hacker Way",
+            "city": "Menlo Park",
+            "postal_code": "94025",
+            "state": "CA",
+            "country": "US"
+        },
+        "summary": {
+            "total_cost": item['price']
+        }
+    }
+}
+
+RECEIPT_ELEMENT_TEMPLATE = lambda item: {
+    "title": item['title'],
+    "quantity": 1,
+    "price": item['price'],
+    "currency": "UAH",
+    "image_url": item['image_url']
+}
+
+QUICK_REPLIES = [
+    {
+        "content_type": "text",
+        "title": "Menu",
+        "payload": {"type": "get_products"}
+    }
+]
+
+PRODUCTS = [{"title": "Classic Fries",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.png?$THUMBNAIL_MEDIUM$",
+             "price": 15},
+            {"title": "Big Mac",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Big-Mac.png?$THUMBNAIL_MEDIUM$",
+             "price": 30},
+            {"title": "Coca-Cola",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Coca-Cola-Classic-Small.png?$THUMBNAIL_MEDIUM$",
+             "price": 10},
+            {"title": "Double Royal Cheesburger",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Double-Quarter-Pounder-with-Cheese.png?$THUMBNAIL_MEDIUM$",
+             "price": 20}]
--- FoodBot/views.py
+++ FoodBot/views.py
@@ -1,18 +1,19 @@
 from FoodBot import app
+from .template_structures import *
 from flask import request
 import requests
 import time
-
 
 @app.route('/', methods=['GET'])
 def handle_verification():
     return request.args.get('hub.challenge') or 'Hello, world'
 
 
-def reply(user_id, msg, type):
+def reply(user_id, payload):
     data = {
         "recipient": {"id": user_id},
-        "message": {"text" if type == 'message' else 'sticker_id': msg}
+        "message": {"attachment": payload,
+                    "quick_replies": QUICK_REPLIES}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
                          json=data)
@@ -21,61 +22,22 @@ def reply(user_id, msg, type):
 
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
-    print(request.json)
     data = request.json
-    sender = data['entry'][0].get('messaging')[0].get('sender').get('id')
-    message = data['entry'][0].get('messaging')[0].get('message').get('text') or data['entry'][0].get('messaging')[
-        0].get('message').get('sticker_id')
-    message_type = "message" if 'sticker_id' not in data['entry'][0].get('messaging')[0].get('message') else "sticker"
-    reply(sender, message, message_type)
-
+    print(data)
+    payload = None
+    if data.get('type'):
+        if data.get('type') == "get_products":
+            payload = PRODUCT_LIST(PRODUCTS)
+        if data.get('type') == "get_receipt":
+            item = find_in_list(data.get("item_name"), PRODUCTS)
+            payload = RECEIPT_TEMPLATE(item)
+        if payload:
+            sender = data['entry'][0].get('messaging')[0].get('sender').get('id')
+            reply(sender, payload)
     return "ok"
 
-
-def make_message_content(data):
-    if 'delivery' in data[0]:
-        return None
-    if 'attachment' in data[0]:
-        pass
-
-
-def make_quick_replies():
-    pass
-
-
-def make_buttons():
-    pass
-
-
-def make_receipt(userid, elements):
-    receipt = {
-        "recipient": {
-            "id": userid
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "receipt",
-                    "recipient_name": "",
-                    "order_number": "",
-                    "currency": "UAH",
-                    "payment_method": "Visa",
-                    "order_url": "",
-                    "timestamp": round(time.time()),
-                    "elements": elements,
-                    "address": {
-                        "street_1": "1 Hacker Way",
-                        "city": "Menlo Park",
-                        "postal_code": "94025",
-                        "state": "CA",
-                        "country": "US"
-                    },
-                    "summary": {
-                        "total_cost": 56.14
-                    }
-                }
-            }
-        }
-    }
-    return receipt
+def find_in_list(title, items):
+    for item in items:
+        if title == item['title']:
+            return item
+    return None
