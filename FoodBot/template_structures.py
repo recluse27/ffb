a9957dc02aee750b7b4bb43eba8ca93d7946901f
arturveres <artur8118@gmail.com>
2017-07-21 16:16:25 +0300

    Fixing mistakes

--- FoodBot/template_structures.py
+++ FoodBot/template_structures.py
@@ -1,80 +1,144 @@
 import time
 
-PRODUCT_LIST = lambda items: {
-    "type": "template",
-    "payload": {
-        "template_type": "list",
-        "elements": [
-            {
-                "title": item['title'],
-                "subtitle": str(item['price']) + ' UAH',
-                "image_url": item['image_url'],
-                "buttons": [
-                    {
-                        "title": "Buy",
-                        "type": "postback",
-                        "payload": "get_product/" + str(items.index(item))
-                    }
-                ]
-            } for item in items]
-    }
-}
-RECEIPT_TEMPLATE = lambda item: {
-    "type": "template",
-    "payload": {
-        "template_type": "receipt",
-        "recipient_name": "Stephane Crozatier",
-        "order_number": round(time.time()),
-        "currency": "UAH",
-        "payment_method": "Credit Card",
-        "order_url": "",
-        "timestamp": round(time.time()),
-        "elements": [
-            RECEIPT_ELEMENT_TEMPLATE(item=item)
-        ],
-        "address": {
-            "street_1": "1 Hacker Way",
-            "city": "Menlo Park",
-            "postal_code": "94025",
-            "state": "CA",
-            "country": "US"
-        },
-        "summary": {
-            "total_cost": item['price']
+
+def PRODUCT_LIST(items):
+    return {
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "elements": [
+                {
+                    "title": item['title'],
+                    "subtitle": str(item['price']) + ' UAH',
+                    "image_url": item['image_url'],
+                    "buttons": [
+                        {
+                            "title": "Buy",
+                            "type": "postback",
+                            "payload": "get_product/" + str(items.index(item))
+                        }
+                    ]
+                } for item in items]
         }
     }
-}
 
-RECEIPT_ELEMENT_TEMPLATE = lambda item: {
-    "title": item['title'],
-    "quantity": 1,
-    "price": item['price'],
-    "currency": "UAH",
-    "image_url": item['image_url']
-}
 
-QUICK_REPLIES = [
-    {
+def RECEIPT_TEMPLATE(item):
+    return {
+        "type": "template",
+        "payload": {
+            "template_type": "receipt",
+            "recipient_name": "Stephane Crozatier",
+            "order_number": round(time.time()),
+            "currency": "UAH",
+            "payment_method": "Credit Card",
+            "order_url": "",
+            "timestamp": round(time.time()),
+            "elements": [
+                RECEIPT_ELEMENT_TEMPLATE(item=item)
+            ],
+            "address": {
+                "street_1": "1 Hacker Way",
+                "city": "Menlo Park",
+                "postal_code": "94025",
+                "state": "CA",
+                "country": "US"
+            },
+            "summary": {
+                "total_cost": item['price']
+            }
+        }
+    }
+
+
+def RECEIPT_ELEMENT_TEMPLATE(item):
+    return {
+        "title": item['title'],
+        "quantity": 1,
+        "price": item['price'],
+        "currency": "UAH",
+        "image_url": item['image_url']
+    }
+
+
+def QUICK_REPLIES_MENU():
+    return {
         "content_type": "text",
         "title": "Menu",
         "payload": "get_all_products"
-    },
-{
-        "content_type": "text",
-        "title": "Choose product",
-        "payload": "get_all_products"
     }
-]
+
+
+def QUICK_REPLIES_GET_MORE(_from, _to):
+    return {
+        "content_type": "text",
+        "title": "Get more",
+        "payload": "get_more/{_from}-{_to}".format(_from=_from,
+                                                   _to=_to)
+    }
+
 
 PRODUCTS = [{"title": "Classic Fries",
-             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.png?$THUMBNAIL_MEDIUM$",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Fries-Small-Medium.png?$THUMBNAIL_MEDIUM$",
              "price": 15},
             {"title": "Big Mac",
-             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Big-Mac.png?$THUMBNAIL_MEDIUM$",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Big-Mac.png?$THUMBNAIL_MEDIUM$",
              "price": 30},
             {"title": "Coca-Cola",
-             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Coca-Cola-Classic-Small.png?$THUMBNAIL_MEDIUM$",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Coca-Cola-Classic-Small.png?$THUMBNAIL_MEDIUM$",
              "price": 10},
             {"title": "Double Royal Cheesburger",
-             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Double-Quarter-Pounder-with-Cheese.png?$THUMBNAIL_MEDIUM$",
-             "price": 20}]
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Double-Quarter-Pounder-with-Cheese.png?$THUMBNAIL_MEDIUM$",
+             "price": 20},
+            {"title": "Maple Bacon Dijon Crispy Chicken Artisan",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-maplebacondijon-crispychicken-artisan.png?$THUMBNAIL_MEDIUM$",
+             "price": 20},
+            {"title": "Sweet BBQ Bacon Crispy Chicken Artisan",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-sweetbbqbacon-crispychicken-artisan.png?$THUMBNAIL_MEDIUM$",
+             "price": 19},
+            {"title": "Picoguac Grilled Chicken Artisan",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-picoguac-grilledchicken-artisan.png?$THUMBNAIL_MEDIUM$",
+             "price": 22},
+            {"title": "Chicken McNuggets",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Chicken-McNuggets-4pc.png?$THUMBNAIL_MEDIUM$",
+             "price": 16},
+            {"title": "Artisan Grilled Chicken Sandwich",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Artisan-Grilled-Chicken-Sandwich.png?$THUMBNAIL_MEDIUM$",
+             "price": 25},
+            {"title": "Premium Bacon Ranch Salad with Grilled Chicken",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Premium-Bacon-Ranch-Salad-with-Grilled-Chicken.png?$THUMBNAIL_MEDIUM$",
+             "price": 17},
+            {"title": "Premium Bacon Ranch Salad with Buttermilk Crispy Chicken",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Premium-Bacon-Ranch-Salad-with-Buttermilk-Crispy-Chicken.png?$THUMBNAIL_MEDIUM$",
+             "price": 17},
+            {"title": "Premium Southwest Salad with Grilled Chicken",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Premium-Southwest-Salad-with-Grilled-Chicken.png?$THUMBNAIL_MEDIUM$",
+             "price": 17},
+            {"title": "Chocolate McCafe Shake",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Chocolate-McCafe-Shake-Medium.png?$THUMBNAIL_MEDIUM$",
+             "price": 12},
+            {"title": "Strawberry McCafe Shake",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Strawberry-McCafe-Shake-Medium.png?$THUMBNAIL_MEDIUM$",
+             "price": 12},
+            {"title": "Vanilla McCafe Shake",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-Vanilla-McCafe-Shake-Medium.png?$THUMBNAIL_MEDIUM$",
+             "price": 12},
+            {"title": "Sweet BBQ Bacon Grilled Chicken Artisan",
+             "image_url": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/"
+                          "desktop/t-mcdonalds-sweetbbqbacon-grilledchicken-artisan.png?$THUMBNAIL_MEDIUM$",
+             "price": 22}]
--- FoodBot/views.py
+++ FoodBot/views.py
@@ -2,7 +2,6 @@ from FoodBot import app
 from .template_structures import *
 from flask import request
 import requests
-import time
 
 
 @app.route('/', methods=['GET'])
@@ -10,55 +9,135 @@ def handle_verification():
     return request.args.get('hub.challenge') or 'Hello, world'
 
 
-def reply_with_attachment(user_id, payload):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"attachment": payload,
-                    "quick_replies": QUICK_REPLIES}
-    }
-    print(data)
-    resp = requests.post("https://graph.facebook.com/v2.9/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
-                         json=data)
-    print(resp.text)
-
-
-def reply_with_message(user_id):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": "Сделай свой выбор",
-                    "quick_replies": QUICK_REPLIES}
-    }
-    resp = requests.post(
-        "https://graph.facebook.com/v2.9/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
-        json=data)
-    print(resp.text)
-
-
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
-    data = request.json['entry'][0].get('messaging')[0]
+    data = request.json.get('entry')[0].get('messaging')[0]
     print(data)
-    payload = None
-    if data and not 'delivery' in data and not 'read' in data and not 'is_echo' in data.get('message', {}):
+    msg_type = ""
+    delimiter = None
+    if check_valid_response(data):
+
         if data.get('message', {}).get('quick_reply'):
+
+            if data.get('message', {}).get('get_started', {}).get('payload') == "get_started":
+                msg_type = "get_started"
+
             if data.get('message', {}).get('quick_reply', {}).get('payload') == "get_all_products":
-                print('product list')
-                payload = PRODUCT_LIST(PRODUCTS)
-            elif 'postback' in data:
-                num = int(data.get('postback', {}).get('payload').split('/')[1])
-                item = PRODUCTS[num]
-                payload = RECEIPT_TEMPLATE(item)
-            if payload:
-                sender = data.get('sender').get('id')
-                reply_with_attachment(sender, payload)
-        else:
+                msg_type = "get_all_products"
+                delimiter = (0, 9)
+
+            if 'postback' in data:
+                if "get_product" in data.get('postback', {}):
+                    msg_type = "get_product"
+                    delimiter = int(data.get('postback', {}).get('payload').split('/')[1])
+
+                if "get_more" in data.get('postback', {}):
+                    msg_type = "get_more"
+                    delimiter = make_delimiter(data.get('postback', {}).get('payload').split('/')[1])
+
             sender = data.get('sender').get('id')
-            reply_with_message(sender)
+            reply(sender, msg_type, delimiter)
+        else:
+            sender = data.get('sender', {}).get('id')
+            reply_with_message(sender, "Сделай заказ", "start_over", delimiter)
+
     return "ok"
 
 
-def find_in_list(title, items):
-    for item in items:
-        if title == item['title']:
-            return item
-    return None
+def construct_quick_replies(msg_type, delimiter=None):
+    quick_replies = dict()
+    if msg_type == 'first_msg' or msg_type == 'star_over':
+        quick_replies.update(QUICK_REPLIES_MENU())
+    if msg_type == 'get_more':
+        quick_replies.update(QUICK_REPLIES_GET_MORE(delimiter[0], delimiter[1]))
+    return [quick_replies]
+
+
+def construct_message_body(msg_type, delimiter=None):
+    payload = dict()
+    if msg_type == "get_all_products" or msg_type == "get_more":
+        payload.update(PRODUCT_LIST(PRODUCTS[delimiter[0]: delimiter[1]]))
+    if msg_type == "get_product":
+        payload.update(RECEIPT_TEMPLATE(PRODUCTS(delimiter)))
+    return payload
+
+
+def make_delimiter(str_from_to):
+    _from = int(str_from_to.split('-')[1]) + 1
+    if len(PRODUCTS[_from:]) > 10:
+        _to = _from + 10
+    else:
+        _to = len(PRODUCTS)
+    return _from, _to
+
+
+def reply(user_id, msg_type, delimiter=None):
+    if msg_type == "get_started":
+        text = ("Привет! Я Friendly Food Bot. С моей помощью ты "
+                "сможешь купить еду в Unit Cafe и подарить ее друзьям.")
+        reply_with_message(user_id, text, msg_type, delimiter)
+
+        text = "Выбери из меню то, что хочешь подарить."
+        reply_with_message(user_id, text, "first_msg", delimiter)
+
+    if msg_type == "get_all_products" or msg_type == 'get_more':
+        reply_with_attachment(user_id, msg_type, delimiter)
+
+    if msg_type == 'get_product':
+        reply_with_attachment(user_id, msg_type, delimiter)
+
+        text = ("Вы только что совершили покупку с помощью Friendly Food Bot. "
+                "Перешлите предыдущее сообщение вашему другу, которого хотите угостить, "
+                "и он сможет получить подарок в любое удобное для него время. "
+                "Friendly Food Bot с удовольствием поможет вам ещё. Для этого "
+                "нужно написать и отправить любое текстовое сообщение в чат с Friendly Food Bot. "
+                "Ждём вас снова! Всего наилучшего!")
+        reply_with_message(user_id, text, "start_over", delimiter)
+
+
+def reply_with_attachment(user_id, msg_type, delimiter=None):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"attachment": construct_message_body(msg_type, delimiter)}
+    }
+
+    quick_replies = construct_quick_replies(msg_type, delimiter)
+    if quick_replies:
+        data.get('message', {}).update({"quick_replies": quick_replies})
+
+    print(data)
+    resp = make_request(data)
+    print(resp.text)
+
+
+def reply_with_message(user_id, text, msg_type, delimiter):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": text}
+    }
+
+    quick_replies = construct_quick_replies(msg_type, delimiter)
+    if quick_replies:
+        data.get('message', {}).update({"quick_replies": quick_replies})
+
+    resp = make_request(data)
+    print(resp.text)
+
+
+def make_request(data):
+    resp = requests.post(
+        "https://graph.facebook.com/v2.9/me/messages?access_token=" + app.config['PAGE_ACCESS_TOKEN'],
+        json=data)
+    return resp
+
+
+def check_valid_response(data):
+    if not data:
+        return False
+    if 'delivery' in data:
+        return False
+    if 'read' in data:
+        return False
+    if 'is_echo' in data.get('message', {}):
+        return False
+    return True
