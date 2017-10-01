c594b75c81dcb09c0b4470de4df6377723f13f8a
arturveres <artur8118@gmail.com>
2017-10-01 20:28:25 +0300

    Loaded back

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from FoodBot import app
 
-app.run(host='127.0.0.1', port=8080)
+app.run(debug=True,
+        host="127.0.0.1",
+        port=8000,
+        use_reloader=True,
+        threaded=True)
