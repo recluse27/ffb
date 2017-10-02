a9b132f1020b70c6dfb3009bbb838a0f4789b312
arturveres <artur8118@gmail.com>
2017-10-03 00:23:51 +0300

    Back to heroku

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from FoodBot import app
 
 app.run(debug=True,
-        host="10.0.0.4",
-        port=443,
+        host="127.0.0.1", #""10.0.0.4",
+        port=8000, #443,
         use_reloader=True,
-        threaded=True,
-        ssl_context=('download_artur8118@gmail.com.pem', 'download_artur8118@gmail.com.pem'))
+        threaded=True)
+        # ssl_context=('download_artur8118@gmail.com.pem', 'download_artur8118@gmail.com.pem'))
