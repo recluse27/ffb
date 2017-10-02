cd2b8fc599d04cf46fe2c6b154166edb3c4e1849
arturveres <artur8118@gmail.com>
2017-10-03 00:21:28 +0300

    http -> https

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from FoodBot import app
 
 app.run(debug=True,
         host="10.0.0.4",
-        port=80,
+        port=443,
         use_reloader=True,
         threaded=True,
         ssl_context=('download_artur8118@gmail.com.pem', 'download_artur8118@gmail.com.pem'))
