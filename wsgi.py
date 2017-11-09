c2ae6571e951daaa58d8b8ca7f63911da57bd069
arturveres <artur8118@gmail.com>
2017-11-10 00:58:55 +0200

    Minor fixes

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
 from FoodBot import app
 
-application = app
+if __name__ == "main":
+    app.run(debug=True,
+            host="10.0.0.4",
+            port=8080,
+            use_reloader=True,
+            threaded=True)
