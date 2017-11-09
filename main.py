bafad6011ce82f8e5a2d02715c6bd5fce15cb776
arturveres <artur8118@gmail.com>
2017-11-10 00:59:40 +0200

    Minor fixes

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from FoodBot import app
 
-
-def get_app():
-    return app
-
-
-if __name__ == "main":
-    app.run(debug=True,
-            host="127.0.0.1",
-            port=8000,
-            use_reloader=True,
-            threaded=True)
+app.run(debug=True,
+        host="127.0.0.1",
+        port=8000,
+        use_reloader=True,
+        threaded=True)
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 from FoodBot import app
 
-if __name__ == "main":
-    app.run(debug=True,
-            host="10.0.0.4",
-            port=8080,
-            use_reloader=True,
-            threaded=True)
+app.run(debug=True,
+        host="10.0.0.4",
+        port=8080,
+        use_reloader=True,
+        threaded=True)
