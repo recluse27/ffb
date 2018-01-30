1bee465f0f9950ceea7f97e7e26d768dbe5c8872
arturveres <artur8118@gmail.com>
2018-01-30 17:03:42 +0200

    Fixing test

--- FoodBot/controller.py
+++ FoodBot/controller.py
@@ -42,7 +42,7 @@ class Controller:
 
             result = []
             items = transform(items_to_show)
-            print(items)
+            print("ITEMS: ", items)
 
             for item in items:
                 if len(item) == 1:
