cbd0b771ae7403b1d4580d799d08ca2e1684a96f
arturveres <artur8118@gmail.com>
2018-01-30 17:10:10 +0200

    Fixing test

--- FoodBot/fb_templates.py
+++ FoodBot/fb_templates.py
@@ -105,7 +105,7 @@ def generic_template(id_type, new_item, button_type=None, **kwargs):
     return template
 
 
-def generic_list_template(id_type, new_items, button_type=None, **kwargs):
+def generic_list_template(id_type, *new_items, button_type=None, **kwargs):
     items = [copy.copy(new_item) for new_item in new_items]
     payload = kwargs
     next_type = id_types.get(id_type, {}).get('next_id')
@@ -120,7 +120,7 @@ def generic_list_template(id_type, new_items, button_type=None, **kwargs):
             "elements": [
                 {
                     "title": item.get('title'),
-                    "subtitle": str(item.get('price')) + ' UAH',
+                    "subtitle": "{price} UAH".format(price=item.get('price'))  if item.get('price') else "",
                     "image_url": item.get('image_url', ''),
                     "buttons": [
                         {
