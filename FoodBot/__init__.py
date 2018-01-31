99a863db80cb6f1e77912f0ffbc06a5ed84e6f97
arturveres <artur8118@gmail.com>
2018-01-31 14:58:29 +0200

    Prod ready

--- FoodBot/__init__.py
+++ FoodBot/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from pymongo import MongoClient
+testing = False
 
 app = Flask('FoodBot')
 app.config.from_object('config')
 app.config.from_envvar('FLASK_TEST_SETTINGS', silent=True)
-testing = True
 mongo = (MongoClient(app.config['MONGO_URI']).heroku_dx30fc17 if not testing
          else MongoClient(app.config["MONGO_TEST"]).heroku_xrfxrbss)
 
