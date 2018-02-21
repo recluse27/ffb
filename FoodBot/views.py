36c9677f281cf294ed3fd9dc9397b08c03d75934
arturveres <artur8118@gmail.com>
2018-02-21 20:07:10 +0200

    New fixes

--- FoodBot/views.py
+++ FoodBot/views.py
@@ -19,7 +19,9 @@ def handle_verification():
 def handle_incoming_messages():
     data = request.json.get('entry', [{}])[0].get('messaging', [{}])[0]
 
+    print(data)
     if controller.is_response_valid(data):
+        print("VALID")
         responses = controller.handle_message(data=data)
 
         for response in responses:
