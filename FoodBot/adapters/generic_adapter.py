1f885b7887580decce25c6219b92b60d68fbfe0d
arturveres <artur8118@gmail.com>
2018-02-21 21:22:28 +0200

    Inheritence rework

--- FoodBot/adapters/generic_adapter.py
+++ FoodBot/adapters/generic_adapter.py
@@ -1,9 +1,15 @@
-from FoodBot.models import Product
+import json
+from datetime import datetime, timedelta
 from typing import Optional, List
+
+import requests as rq
+
+from FoodBot.constants import CACHE_UPDATE_DAYS, HEADERS
+from FoodBot.models import Product, BotOrder
+from FoodBot.utils import get_or_create_order
 
 
 class IAdapter:
-
     name = None
     url = None
     image_url = None
@@ -23,17 +29,81 @@ class IAdapter:
     def checkout(self, **kwargs) -> dict:
         raise NotImplementedError
 
-    def add_product(self, **kwargs) -> str:
-        raise NotImplementedError
+    def get_categories(self, **kwargs) -> List[dict]:
+        expire_date = (self.cached_categories_updated + timedelta(days=CACHE_UPDATE_DAYS)
+                       if self.cached_categories_updated else None)
 
-    def get_product_by_id(self, id) -> Optional[Product]:
-        raise NotImplementedError
+        if not self.cached_categories or (expire_date and expire_date < datetime.utcnow()):
+            self.get_categories_from_api()
 
-    def remove_product(self, **kwargs) -> str:
-        raise NotImplementedError
+        categories = [category.to_json() for category in self.cached_categories]
+
+        return categories
 
     def get_products(self, **kwargs) -> List[dict]:
-        raise NotImplementedError
+        expire_date = (self.cached_products_updated + timedelta(days=CACHE_UPDATE_DAYS)
+                       if self.cached_products_updated else None)
+        category_id = kwargs.get('id')
+        if not self.cached_products or (expire_date and expire_date < datetime.utcnow()):
+            self.get_products_from_api()
 
-    def get_categories(self, **kwargs) -> List[dict]:
-        raise NotImplementedError
+        products = [product.to_json() for product in self.cached_products if product.category_id == category_id]
+
+        return products
+
+    def get_product_by_id(self, product_id) -> Optional[Product]:
+        expire_date = (self.cached_products_updated + timedelta(days=CACHE_UPDATE_DAYS)
+                       if self.cached_products_updated else None)
+
+        if not self.cached_products or (expire_date and expire_date < datetime.utcnow()):
+            self.get_products_from_api()
+
+        result = list(filter(lambda product: product.id == product_id, self.cached_products))
+        return result[0] if result else None
+
+    def is_product_available(self, product_id) -> bool:
+        result = rq.get(url=(self.url % 'product/{id}'.format(id=product_id)),
+                        headers=HEADERS)
+        try:
+            product_data = json.loads(result.text)[0]
+            return bool(product_data.get('status'))
+        except TypeError:
+            return False
+
+    def add_product(self, **kwargs) -> str:
+        if not self.cached_products:
+            self.get_products_from_api()
+
+        sender = kwargs.get('user_id')
+        provider = kwargs.get('provider')
+        product_id = kwargs.get('id')
+        user_order = get_or_create_order(BotOrder, sender, provider)
+
+        product = self.get_product_by_id(product_id)
+        if not self.is_product_available(product_id) or product is None:
+            return "Продукт наразі недоступний."
+
+        orders = user_order.orders
+        orders.append(product.to_json())
+        user_order.orders = orders
+        user_order.commit()
+        return "Додано {title}.".format(title=product.title)
+
+    def remove_product(self, **kwargs) -> str:
+        if not self.cached_products:
+            self.get_products_from_api()
+
+        sender = kwargs.get('user_id')
+        provider = kwargs.get('provider')
+        product_id = kwargs.get('id')
+        user_order = get_or_create_order(BotOrder, sender, provider)
+
+        product = self.get_product_by_id(product_id)
+        if product is None:
+            return "Продукт наразі недоступний."
+
+        orders = user_order.orders
+        orders.remove(product.to_json())
+        user_order.orders = orders
+        user_order.commit()
+        return "Видалено {title}.".format(title=product.title)
--- FoodBot/adapters/unit_adapter.py
+++ FoodBot/adapters/unit_adapter.py
@@ -3,12 +3,9 @@ from datetime import datetime, timedelta, timezone
 
 import requests as rq
 
-from typing import Optional, List
-
 from FoodBot.adapters.generic_adapter import IAdapter
-from FoodBot.constants import HEADERS, CACHE_UPDATE_DAYS
-from FoodBot.models import Product, Category, BotOrder
-from FoodBot.utils import get_or_create_order
+from FoodBot.constants import HEADERS
+from FoodBot.models import Product, Category
 
 
 class UnitAdapter(IAdapter):
@@ -64,82 +61,3 @@ class UnitAdapter(IAdapter):
 
         self.cached_products = new_products
         self.cached_products_updated = datetime.utcnow()
-
-    def get_categories(self, **kwargs) -> List[dict]:
-        expire_date = (self.cached_categories_updated + timedelta(days=CACHE_UPDATE_DAYS)
-                       if self.cached_categories_updated else None)
-
-        if not self.cached_categories or (expire_date and expire_date < datetime.utcnow()):
-            self.get_categories_from_api()
-
-        categories = [category.to_json() for category in self.cached_categories]
-
-        return categories
-
-    def get_products(self, **kwargs) -> List[dict]:
-        expire_date = (self.cached_products_updated + timedelta(days=CACHE_UPDATE_DAYS)
-                       if self.cached_products_updated else None)
-        category_id = kwargs.get('id')
-        if not self.cached_products or (expire_date and expire_date < datetime.utcnow()):
-            self.get_products_from_api()
-
-        products = [product.to_json() for product in self.cached_products if product.category_id == category_id]
-
-        return products
-
-    def get_product_by_id(self, id) -> Optional[Product]:
-        expire_date = (self.cached_products_updated + timedelta(days=CACHE_UPDATE_DAYS)
-                       if self.cached_products_updated else None)
-
-        if not self.cached_products or (expire_date and expire_date < datetime.utcnow()):
-            self.get_products_from_api()
-
-        result = list(filter(lambda product: product.id == id, self.cached_products))
-        return result[0] if result else None
-
-    def is_product_available(self, product_id) -> bool:
-        result = rq.get(url=(self.url % 'product/{id}'.format(id=product_id)),
-                        headers=HEADERS)
-        try:
-            product_data = json.loads(result.text)[0]
-            return bool(product_data.get('status'))
-        except TypeError:
-            return False
-
-    def add_product(self, **kwargs) -> str:
-        if not self.cached_products:
-            self.get_products_from_api()
-
-        sender = kwargs.get('user_id')
-        provider = kwargs.get('provider')
-        product_id = kwargs.get('id')
-        user_order = get_or_create_order(BotOrder, sender, provider)
-
-        product = self.get_product_by_id(product_id)
-        if not self.is_product_available(product_id) or product is None:
-            return "Продукт наразі недоступний."
-
-        orders = user_order.orders
-        orders.append(product.to_json())
-        user_order.orders = orders
-        user_order.commit()
-        return "Додано {title}.".format(title=product.title)
-
-    def remove_product(self, **kwargs) -> str:
-        if not self.cached_products:
-            self.get_products_from_api()
-
-        sender = kwargs.get('user_id')
-        provider = kwargs.get('provider')
-        product_id = kwargs.get('id')
-        user_order = get_or_create_order(BotOrder, sender, provider)
-
-        product = self.get_product_by_id(product_id)
-        if product is None:
-            return "Продукт наразі недоступний."
-
-        orders = user_order.orders
-        orders.remove(product.to_json())
-        user_order.orders = orders
-        user_order.commit()
-        return "Видалено {title}.".format(title=product.title)
